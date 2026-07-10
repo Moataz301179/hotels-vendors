964223dc84d0c47e27e4a568f10903c033876c89
Moataz Ghani <pss.alexandria@gmail.com>
2026-07-10 06:17:35 +0300

    test: simplify marketing page to server-import to isolate client crash

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,16 +1,5 @@
-"use client"
-
-import dynamic from "next/dynamic"
-
-const MarketingPage = dynamic(() => import("./page.client"), {
-  ssr: false,
-  loading: () => (
-    <div style={{ minHeight: "100vh", display: "flex", alignItems: "center", justifyContent: "center", background: "#000" }}>
-      <p style={{ color: "#fff" }}>Loading HotelsVendors...</p>
-    </div>
-  ),
-})
+import MarketingPage from "./page.client";
 
 export default function Page() {
-  return <MarketingPage />
+  return <MarketingPage />;
 }
