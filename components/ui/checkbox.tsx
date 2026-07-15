8183365a1a24ca98cf8ff1e887949e13c1ff6f17
Moataz Ghani <pss.alexandria@gmail.com>
2026-07-15 09:08:00 +0300

    feat: add Checkbox component for factoring activation page

---
+++ components/ui/checkbox.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import * as React from "react"
+import * as CheckboxPrimitive from "@radix-ui/react-checkbox"
+import { cn } from "@/lib/utils"
+
+const Checkbox = React.forwardRef<
+  React.ComponentRef<typeof CheckboxPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
+>(({ className, ...props }, ref) => (
+  <CheckboxPrimitive.Root
+    ref={ref}
+    className={cn(
+      "peer h-4 w-4 shrink-0 rounded-sm border border-white/20 bg-white/5 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-[#39ff7e] disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-[#39ff7e] data-[state=checked]:text-black",
+      className
+    )}
+    {...props}
+  >
+    <CheckboxPrimitive.Indicator className="flex items-center justify-center text-current">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" className="h-3 w-3"><polyline points="20 6 9 17 4 12"></polyline></svg>
+    </CheckboxPrimitive.Indicator>
+  </CheckboxPrimitive.Root>
+))
+Checkbox.displayName = CheckboxPrimitive.Root.displayName
+
+export { Checkbox }
