e7a8acad9fd59eb8563554834ff9b285ada24486
deploy <operations@hotelsvendors.com>
2026-08-26 02:52:54 +0000

    fix: HeroSection import + homepage cleanup

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,15 +1,9 @@
 import { HeroSection } from './components/HeroSection';
-import { SolutionsSection } from './components/SolutionsSection';
-import { StatsSection } from './components/StatsSection';
-import { CTASection } from './components/CTASection';
 
 export default function HomePage() {
   return (
     <main className="min-h-screen">
       <HeroSection />
-      <SolutionsSection />
-      <StatsSection />
-      <CTASection />
     </main>
   );
 }
